b828003fd7a1166b1dfee797ca3872b03f117f0b
tartartar <112954170+itsChewin@users.noreply.github.com>
2024-06-22 12:27:11 +0700

    [EDIT] fix dev

--- watercactus_backend/index.js
+++ watercactus_backend/index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const mysql = require('mysql');
-const cors = require('cors');
+const express = require("express");
+const mysql = require("mysql");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
@@ -11,25 +11,25 @@ app.use(express.json());
 app.use(bodyParser.json({ type: "application/json" }));
 
 app.use(
-    cors({
-        origin: '*',
-        credentials: true,
-    })
+  cors({
+    origin: "*",
+    credentials: true,
+  })
 );
 
-require('dotenv').config();
+require("dotenv").config();
 
 const connection = mysql.createConnection({
   host: process.env.DATABASE_URL,
   port: process.env.PORT,
   user: process.env.DATABASE_USER,
   password: process.env.PASSWORD,
-  database: process.env.DATABASE_NAME
+  database: process.env.DATABASE_NAME,
 });
 
 connection.connect();
 global.connection = connection;
-console.log("Database is connected");;
+console.log("Database is connected");
 
 app.get("/", (req, res) => {
   res.send("Hello WaterCactus!");
